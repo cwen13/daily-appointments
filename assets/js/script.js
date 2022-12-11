c6dd73db0d3617aae1cfac84a97be022164cae20
cwen13 <cwenrich13@gmail.com>
2022-12-11 07:13:28 -0500

    appointment functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,10 @@
 let saveBtn = $(".saveBtn");
-let timeBlocks = $("time-block");
+let timeBlocks = $(".time-block");
 let appointments = {};
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
-
-  function loadAppointments () {
-    appointments = localStorage.getItem("appointments");
-    appointments = appointments ? JSON.parse(appointments) : {};
-    return 0;
-  }
 
   function displayAppointments () {
     for (const hour in appointments) {
@@ -38,20 +32,33 @@ $(function () {
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
+  function clearTimeClass() {
+    for (let i=0;i<timeBlocks.length;i++) {
+   
+      if (timeBlocks[i].hasClass("past")) {
+	timeBlocks[i].addRemove("past");
+      } else if(timeBlocks[i].hasClass("present")) {
+	timeBlocks[i].addRemove("present");
+      } else {
+	timeBlocks[i].addRemove("future");
+      }
+    }
+    return 0;
+  }
+
   function  updateTimeColor() {
     // run every minute to update colors
     let currentHour = parseInt(dayjs().format("HH"));
-
     
-    for (const tBlock in timeBlocks) {
-      let blockHour = parseInt(tBlock.attr("id").slice(-2));
+    for (let i=0;i,timeBlocks.length;i++) {
+      let blockHour = parseInt(timeBlocks[i].getAttribute("id").slice(-2));
       
       if (currentHour < blockHour) {
-	  tBlock.addClass("past");
-      } else if() {
-	tBlock.addClass("present");
+	timeBlocks[i].addClass("past");
+      } else if(currentHour === blockHour) {
+	timeBlocks[i].addClass("present");
       } else {
-	tBlock.addClass("future");
+	timeBlocks[i].addClass("future");
       }
     }
     return 0;
@@ -61,10 +68,29 @@ $(function () {
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  function fetchAppointment(){
+  function saveAppointmentsToStorage(appointments) {
+    localStorage.setItem(appointments);
+  }
+  
+  function loadAppointmentsFromStorage () {
+    appointments = localStorage.getItem("appointments");
+    appointments = appointments ? JSON.parse(appointments) : {};
+    return 0;
+  }
+
+  function populateAppointment(){
     
   }
 
+  function handleAppointmentSave(event) {
+    let appointment = $(this +" textarea")
+    let appointmentHour = parseInt($(this).attr("id"));
+    let appointments = loadAppointmentsFromStorage();
+    appoointments[appointmentHour-9] += "n/" + appointment.val();
+    
+    return 0;
+  }
+  
   // TODO: Add code to display the current date in the header of the page.
   function displayTime() {
     let todayDate = dayjs().format("YYYY MMMM DD, dddd HHmm");
@@ -79,8 +105,9 @@ $(function () {
   }
 
   
-  saveBtn.on("click", saveAppointment);
-  updateTimeColors();
+  saveBtn.on("click", saveAppointmentsToStorage);
+//  clearTimeClass();
+//  updateTimeColor();
   displayTime();
   setInterval(updateTime, 60000)  
 
